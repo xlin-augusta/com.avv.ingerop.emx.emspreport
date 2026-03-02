ac8d4cddee411bf47faf982d9dfab22e0f6e94db
Naoures-BHaj <nbenelhajsalah@augustareeves.fr>
2026-03-02 09:38:14 +0000

    Add navigation external to FGA

--- webapp/ext/controller/AnalyticalListExtend.controller.js
+++ webapp/ext/controller/AnalyticalListExtend.controller.js
@@ -14,7 +14,34 @@ sap.ui.define(
             // Override or add custom methods here
 
             // this section allows to extend lifecycle hooks or hooks provided by Fiori elements
-            override: {
+            override:    
+            {
+                  onListNavigationExtension(oEvent, oBindingContext, bReplaceInHistory) {
+                    // oEvent, oBindingContext, bReplaceInHistory
+                    try {
+                        const { BusinessNo } = oBindingContext.getObject();
+                        const periodId = "com.avv.ingerop.emx.emspreport::sap.suite.ui.generic.template.AnalyticalListPage.view.AnalyticalListPage::ZC_EMSP_REPORTResults--template::SmartFilterBar-filterItemControlA_-_Parameter.p_period";
+                        const p_period = this.getView().byId(periodId).getValue();
+                        const oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+
+                        const sHash = oCrossAppNavigator.hrefForExternal({
+                            target: {
+                                semanticObject: "ZFGA",
+                                action: "manage"
+                            },
+                            params: {
+                                p_period,
+                                BusinessNo
+                            }
+
+                        });
+
+                        window.open(sHash, "_blank", "noopener,noreferrer");
+
+                    } catch (err) {
+                        console.error("Error during navigation:", err);
+                    }
+                }
                 // onInit: function (oEvent) {
                 //     console.log("onInit called");
                 //     // this.getInterface().getView().getController().extensionAPI.attachPageDataLoaded(this._onObjectExtMatched.bind(this));
